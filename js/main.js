f687b4bcc0197b42c4bad1533dce3ac9b7b5f1df
nemazeelaura <arvadajohnson@gmail.com>
2017-12-11 16:32:14 -0700

    upload files

--- js/main.js
+++ js/main.js
@@ -1,15 +1,62 @@
+// $(document).ready(function() {
+//     // alert("ready");
+//     // $('table').on('click', 'id[]', function(e) {
+// 	// });
+// 	var next = "X";
+// 	var x = "X";
+// 	var o = "O";
+// 	$(".box").click(function() { 
+// 		$(this).html(x);
+// 	});  
+
+// });
+
 $(document).ready(function() {
     // alert("ready");
 
     // $('table').on('click', 'id[]', function(e) {
- 
-	// });
-	var x = "X";
-	var o = "O";
-	$(".box").click(function() { 
-		$(this).html(x);
-	});  
-	
+
+    // });
+    var next = 1;
+    var x = "X";
+    var o = "O";
+    var boxes = [];
+
+// EXAMPLE accessing values in multidimentional array boxes.row1.key1 == 'input1';
+
+
+    $(".box").click(function() {
+        var selectedBox = this.id;
+        //
+        var clicked = 0;
+        for(var i = 0; i < boxes.length; i++){
+            if(boxes[i] == selectedBox) clicked = 1;
+        }
+        
+        if(clicked == 0) {
+            boxes.push(selectedBox);
+
+            var letterToUse;
+            if(next){
+                letterToUse = x;
+                next = 0;
+                $(this).addClass("xColor");
+            }
+            else {
+                letterToUse = o;
+                next = 1;
+                $(this).addClass("oColor");
+            }
+
+            $(this).html(letterToUse);
+
+        } // END if(clicked)
+
+    });
+    $("#clearGame").click( function()
+           {
+                   location.reload();
+           }
+        );
+
 });
-
-
